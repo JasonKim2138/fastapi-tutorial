f91feda4183d3d4d9dfd32de134370917fabe35d
jasonkim <vzv6na@virginia.edu>
2024-12-09 18:59:02 -0500

    created tables, get information of tables, updated tables, deleted tables

--- myenv/main.py
+++ myenv/main.py
@@ -1,19 +1,69 @@
 from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
 students = {
     1: {
-        "name": "jason",
-        "age": "23",
-        "what": "the fuck"
+        "name": "Jason",
+        "age": 23,
+        "year": 10
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: int
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[int] = None
 
 @app.get("/")
 def read():
     return {"hello": "world"}
 
 @app.get("/student/{student_id}")
-def get_student(student_id: int):
+def get_student(student_id: int = Path(description="The ID of the student you want to view",gt=0)):
     return students[student_id]
+
+@app.get("/get-by-name")
+def get_student(*, student_id : int, name: Optional[str] = None, test : int):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+        return {"Data": "Not found"}
+    
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student: Student):
+    if student_id in students:
+        return {"Error": "Student exists"}
+    students[student_id] = student
+    return students[student_id]
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student doesn't exist"}
+    if student.name != None:
+        students[student_id].name = student.name
+    if student.age != None:
+        students[student_id].age = student.age
+    if student.year != None:
+        students[student_id].year = student.year
+    return students[student_id]
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student doesn't exist"}
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
+
+    
+
+    
